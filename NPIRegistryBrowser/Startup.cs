0a294575755dff764c022af8ba45192ff4f3907e
Edsel Clarin <edsel.clarin@yahoo.com>
2022-09-10 14:48:40 -0700

    Add project files.

---
+++ NPIRegistryBrowser/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Caliburn.Micro;
+using NPIRegistryBrowser.ViewModels;
+using System.Windows;
+
+namespace NPIRegistryBrowser
+{
+    internal class Startup : BootstrapperBase    
+    {
+        public Startup()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewForAsync<MainViewModel>();
+        }
+    }
+}
